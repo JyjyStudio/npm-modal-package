82e43804ba46165be4e7a727af650ea280d6d2ce
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-31 17:53:09 +0100

    feat: clean code + add dropdown component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,65 +1,55 @@
 import styled from 'styled-components'
-import { addEmployee } from '../features/employees/employeesSlice'
-import { useTsDispatch } from '../utils/redux/hooks'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import Modal from '../components/Modal'
+import Input from '../components/Form/Input'
+import StateSelect from '../components/Form/SelectState'
+import DatePicker from '../components/Form/DatePicker'
+import Dropdown from '../components/Form/Dropdown'
+
 
 export default function Home(): JSX.Element {
-	
-	const [modal, setModal] = useState(false)
 
-	const dispatch = useTsDispatch()
+	// local state to store every input value
+	const [modal, setModal] = useState(false)
+	const [firstname, setFirstname] = useState("")
+	const [lastname, setLastname] = useState("")
+	const [street, setStreet] = useState("")
+	const [city, setCity] = useState("")
+	const [state, setState] = useState("")
+	const [zipCode, setZipCode] = useState("")
 
 	const handleSubmit = (event: React.ChangeEvent<HTMLFormElement>) => {
 		event.preventDefault()
-		dispatch(addEmployee())
 		setModal(true)
 	}
 
+
 	return (
 		<Container>
+
 			<h1>HRnet</h1>
-			<Link to="list">View Current Employees</Link>
+			<StyledLink to="list">View Current Employees</StyledLink>
 			<h2>Create Employee</h2>
-			<Form action="#" id="create-employee" onSubmit={handleSubmit}>
-				<label htmlFor="first-name">First Name</label>
-				<input type="text" id="first-name" />
 
-				<label htmlFor="last-name">Last Name</label>
-				<input type="text" id="last-name" />
+			<Form onSubmit={handleSubmit}>
 
-				<label htmlFor="date-of-birth">Date of Birth</label>
-				<input id="date-of-birth" type="text" />
-
-				<label htmlFor="start-date">Start Date</label>
-				<input id="start-date" type="text" />
-
-				<fieldset className="address">
+				<Input type='text' name='first-name' value={firstname} setFunction={setFirstname} required/>
+				<Input type='text' name='last-name' value={lastname} setFunction={setLastname} required/>
+				<DatePicker label='date-of-birth' maxDate={new Date()} required />
+				<DatePicker label='start-date' required />
+				<Fieldset>
 					<legend>Address</legend>
-
-					<label htmlFor="street">Street</label>
-					<input id="street" type="text" />
-
-					<label htmlFor="city">City</label>
-					<input id="city" type="text" />
-
-					<label htmlFor="state">State</label>
-					<select name="state" id="state"></select>
-
-					<label htmlFor="zip-code">Zip Code</label>
-					<input id="zip-code" type="number" />
-				</fieldset>
-
-				<label htmlFor="department">Department</label>
-				<select name="department" id="department">
-					<option>Sales</option>
-					<option>Marketing</option>
-					<option>Engineering</option>
-					<option>Human Resources</option>
-					<option>Legal</option>
-				</select>
-				<button type="submit">Save</button>
+					<Input type='text' name='street' value={street} setFunction={setStreet} required/>
+					<Input type='text' name='city' value={city} setFunction={setCity} required/>
+					<StateSelect value={state} setFunction={setState}/>
+					<Input type='number' name='zip-code' value={zipCode} setFunction={setZipCode} min={0} required/>
+				</Fieldset>
+				
+				<Dropdown />
+				
+				<SubmitBtn type="submit">Save</SubmitBtn>
+				
 			</Form>
 
 			{modal && <Modal setModal={setModal}>Employee Created Successfully !</Modal>}
@@ -72,8 +62,32 @@ const Container = styled.main`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
+	gap: 0.7rem;
+	background: url()
+`
+const StyledLink = styled(Link)`
+	color: inherit;
+	text-decoration: none;
+	&:hover {
+		text-decoration: underline;
+	}
 `
 const Form = styled.form`
 	display: flex;
 	flex-direction: column;
+	border: 2px solid #c0c4d6;
+	padding: 1rem;
+	margin: 1rem;
+	width: 50%;
+	@media screen and (max-width: 700px) {
+		width: 90%
+	}
+`
+const Fieldset = styled.fieldset`
+	margin-top: 10px;
+	padding: 5px 10px 10px;
+	border: 1px solid #c0c4d6;
+`
+const SubmitBtn = styled.button`
+	margin-top: 1rem;
 `
