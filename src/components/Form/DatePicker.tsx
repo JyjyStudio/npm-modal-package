2ea2cfa0903e284a1650fab38e0e58240d5db1bc
Jyjystudio <jyhad.benyoussef@gmail.com>
2022-10-31 18:23:47 +0100

    feat: DatePicker component

---
+++ src/components/Form/DatePicker.tsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import type { Value } from 'react-multi-date-picker'
+import DatePicker from 'react-multi-date-picker'
+import transition from 'react-element-popper/animations/transition'
+import InputIcon from "react-multi-date-picker/components/input_icon"
+
+export default function DPicker({ label, maxDate, required }: Props) {
+	
+	const formatLabel = (label: string) => label.split('-').map((word) => word.charAt(0).toUpperCase() + word.slice(1) + ' ')
+	const [date, setDate] = useState<Value>(new Date())
+
+	return (
+		<>
+			<label htmlFor={label}>{formatLabel(label)}</label>
+			<DatePicker
+				value={date}
+				onChange={setDate}
+				id={label}
+				maxDate={maxDate && maxDate}
+				required={required && required}
+				calendarPosition="bottom-end"
+				animations={[transition()]} 
+				render={<InputIcon/>}
+				fixMainPosition={true}
+			/>
+		</>
+	)
+}
+
+interface Props {
+	label: string
+	maxDate?: Date
+	required?: boolean
+}
