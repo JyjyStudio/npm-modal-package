b10096f661ca60c73a7263ba3c97f970cbe8294b
JyjyStudio <jyhad.benyoussef@gmail.com>
2022-12-17 15:24:07 +0100

    feat: Modal component with useModal hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import { createPortal } from 'react-dom'
 import styled from 'styled-components'
 
-export const Modal:React.FC<Props> = ({ children, setModal, overlay_color="rgba(49,49,49,0.8)", modal_color="white", text_color="black" }: Props): JSX.Element => {
-	return (
-		<div className='modal'>
-			<Overlay onClick={() => setModal(false)} overlay_color={overlay_color}/>
-			<Content modal_color={modal_color}>
-				<CloseIcon onClick={() => setModal(false)}>X</CloseIcon>
-				<p style={{color: text_color}}>{children}</p>
-			</Content>
-		</div>
-	)
-}
+export const Modal: React.FC<Props> = ({
+	children,
+	overlay_color = 'rgba(49,49,49,0.8)',
+	modal_color = 'white',
+	text_color = 'black',
+	visible,
+	hide,
+}) =>
+	visible
+		? createPortal(
+				<div className="modal">
+					<Overlay onClick={hide} overlay_color={overlay_color} />
+					<Content modal_color={modal_color}>
+						<CloseIcon onClick={hide}>X</CloseIcon>
+						<p style={{ color: text_color }}>{children}</p>
+					</Content>
+				</div>,
+				document.body
+		  )
+		: null
 
 export default Modal
 export interface Props {
 	children: string
-	setModal: React.Dispatch<React.SetStateAction<boolean>>
+	visible: boolean
+	hide: () => void
 	overlay_color?: string
 	modal_color?: string
 	text_color?: string
@@ -40,20 +51,21 @@ const Overlay = styled.div<Overlay>`
 const Content = styled.div<Content>`
 	position: absolute;
 	width: 50%;
-	background-color: ${({modal_color}) => modal_color};
+	background-color: ${({ modal_color }) => modal_color};
 	border-radius: 5px;
 	text-align: center;
 	padding: 2rem 0;
 	top: 40%;
 	left: 50%;
-    transform: translateX(-50%);
+	transform: translateX(-50%);
 	@media screen and (max-width: 500px) {
-	    width: 60%;
-    	padding: 4rem 1rem;
-	    font-size: 1.2rem;
+		width: 60%;
+		padding: 4rem 1rem;
+		top: 60%;
+		font-size: 1.2rem;
 	}
 `
-const CloseIcon = styled.span`
+const CloseIcon = styled.button`
 	position: absolute;
 	top: -10px;
 	right: -10px;
